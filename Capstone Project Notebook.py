f2ded1dac5c9eac7f8f76e77f035febbb4edcaaa
DerekJRW <62646915+DerekJRW@users.noreply.github.com>
2020-03-25 19:53:41 +0800

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is primarily write for the IBM Data Science Capstone Project.
+# Here is the assignment for course 9, week 1.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
